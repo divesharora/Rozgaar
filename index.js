e8a502e75f6f2c17aefaa92db7658beb823b197b
Abhishek Padhy <abhishekpadhyakr@gmail.com>
2020-09-06 22:20:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -24,3 +24,52 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+// contact form validation and other effects
+
+function validatenoblank()
+        {
+            var username=document.getElementById("uname");
+            var useremail=document.getElementById("uemail");
+            var usermessage=document.getElementById("umessage");
+            if(username.value.trim()=="")
+            {
+                alert("Blank username");
+                username.style.border="solid 1px #f8bd7f";
+                return false;
+            }
+            else if(useremail.value.trim()=="")
+            {
+                alert("Blank email");
+				useremail.style.border="solid 1px #f8bd7f";
+				username.style.border="solid 1px black";
+                return false;
+            }
+            else if(usermessage.value.trim()=="")
+            {
+                alert("Blank message");
+				usermessage.style.border="solid 1px #f8bd7f";
+				useremail.style.border="solid 1px black";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        function validateemail()
+        {
+            var useremail=document.getElementById("uemail").value;
+            var regx=/^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+            if(regx.test(useremail))
+            {
+                alert("Valid email");
+                return true;
+            }
+            else
+            {
+                alert("Invalid email");
+                return false;
+            }
+
+        }
